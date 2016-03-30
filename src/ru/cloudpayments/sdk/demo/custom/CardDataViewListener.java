001f05d5c28109111e33b8a617c66a4645f2e574
anastasia <anastasiapleshkan@gmail.com>
2016-03-30 19:29:45 +0300

    Добавлен выбор валюты GUI

---
+++ src/ru/cloudpayments/sdk/demo/custom/CardDataViewListener.java
@@ -0,0 +1,10 @@
+package ru.cloudpayments.sdk.demo.custom;
+
+import java.lang.String; /**
+ * Created by Nastya on 29.09.2014.
+ */
+public interface CardDataViewListener {
+
+    void makePayment(String cardNumber, String expDate, String cvv, String holderName, double amount, String currency, String desc);
+
+}
